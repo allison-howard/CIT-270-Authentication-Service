2e0b6b373458dd46fbbf61f7e7fe2b430d9e3a8d
Allison <obiwankanoboe@gmail.com>
2022-11-02 19:08:00 +0000

    Week 8 Push from Cloud Shell

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const md5 = require('md5');
 
 const redisClient = createClient(
 {
-    Url:'redis://default@localhost:6379',
+    url:'redis://default@34.172.150.215:6379',
 }
 );
 
@@ -21,8 +21,14 @@ https.createServer({
     cert: fs.readFileSync('server.cert'),
     // passphrase: 'P@ssw0rd'
 }, app).listen(port,async () => {
+    try {
     await redisClient.connect();
     console.log('Listening...');
+    }
+    catch(error) {
+        console.log(error);
+    }
+    
 });
 
 // app.listen(port, async ()=>{
